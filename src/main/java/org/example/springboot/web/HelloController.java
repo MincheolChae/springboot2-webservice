526044c1537f8ebb0c3f41e4c08066461474415d
MincheolChae <cmc752@gmail.com>
2020-06-17 14:39:26 +0900

    20.06.17

--- src/main/java/org/example/springboot/web/HelloController.java
+++ src/main/java/org/example/springboot/web/HelloController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     //이부분은 HelloControllerTest 할때만 주석해제하고 Application 실행시에는 주석처리해줘야 함
-    @GetMapping
-    public String hello(){
-        return "hello";
-    }
+//    @GetMapping
+//    public String hello(){
+//        return "hello";
+//    }
 
     @GetMapping("/hello/dto")
     public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
